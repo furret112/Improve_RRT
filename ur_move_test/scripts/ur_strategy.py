374fa0c6fe57eacb26df548c4a684e882de3f9bd
furret112 <furret112@gmail.com>
2022-01-03 17:46:22 +0800

    2022/01/03 -- finish strategy

--- ur_move_test/scripts/ur_strategy.py
+++ ur_move_test/scripts/ur_strategy.py
@@ -51,17 +51,21 @@ class MoveGroupTutorial(object):
     display_trajectory_publisher = rospy.Publisher('/move_group/display_planned_path',
                                                    moveit_msgs.msg.DisplayTrajectory,
                                                    queue_size=20)
-    reference_frame = 'base_link'
+    reference_frame = '/base_link'
     group.set_pose_reference_frame(reference_frame)
     ee_link = group.get_end_effector_link()
     
     group.set_end_effector_link(ee_link)
 
-    group.set_planner_id("RRTConnect")
-    group.set_planning_time(10)
+    #group.set_planner_id("CSAGO_RRT")
+    group.set_planner_id("CSAGO_RRT")
+    # group.set_planning_time(10)
+    # group.set_goal_position_tolerance(0.1)
+    # group.set_goal_orientation_tolerance(0.1)
     group.set_max_acceleration_scaling_factor(0.1)
     group.set_max_velocity_scaling_factor(0.1)
-    
+
+
     self.box_name = ''
     self.robot = robot
     self.scene = scene
@@ -80,7 +84,8 @@ class MoveGroupTutorial(object):
     group.set_max_acceleration_scaling_factor(0.1)
     group.set_max_velocity_scaling_factor(0.1)
     joint_goal = group.get_current_joint_values()
-    joint_goal[0] = -pi*3/2
+    # joint_goal[0] = -pi*3/2
+    joint_goal[0] =  pi/2
     joint_goal[1] = -pi/2
     joint_goal[2] =  pi/2
     joint_goal[3] = -pi/2
@@ -91,8 +96,11 @@ class MoveGroupTutorial(object):
     group.clear_pose_targets()
     current_joints = group.get_current_joint_values()
     origin_orientation =  group.get_current_pose().pose.orientation
-    origindegree =  euler_from_quaternion([origin_orientation.x, origin_orientation.y, origin_orientation.z, origin_orientation.w]) 
 
+    origin =  group.get_current_pose(self.end_effector_link).pose
+    print(origin)
+    origindegree =  euler_from_quaternion([origin_orientation.x, origin_orientation.y, origin_orientation.z, origin_orientation.w]) 
+    print(origindegree)
     
     self.origin_degree[0] = origindegree[0]/3.14*180.0
     self.origin_degree[1] = origindegree[1]/3.14*180.0
@@ -114,61 +122,49 @@ class MoveGroupTutorial(object):
     pose_goal.position.y = (487 -70 + (obj_y*2.27))/1000 #0.630
     pose_goal.position.z = 0.146  #0.106  #0.0823
 
+    pose_goal.position.x = pose_goal.position.x + (0.015)
+    pose_goal.position.y = pose_goal.position.y + 0.145
+    pose_goal.position.z = pose_goal.position.z + 1.03
+
+
     calibrate_x = ((234.16 - obj_x)*4.16 -108.8)/1000
     calibrate_y = (487 -70 + (obj_y*2.27))/1000
 
     
     quaternion = quaternion_from_euler(np.radians(-180),np.radians(0), np.radians(-90))   #roll_angle, pitch_angle, yaw_angle  
-    
 
 
     pose_goal.orientation.x = quaternion[0]
+    print(pose_goal.orientation.x)
     pose_goal.orientation.y = quaternion[1]
+    print(pose_goal.orientation.y)
     pose_goal.orientation.z = quaternion[2]
+    print(pose_goal.orientation.z)
     pose_goal.orientation.w = quaternion[3]
+    print(pose_goal.orientation.w)
+
 
     group.set_pose_target(pose_goal, self.end_effector_link)
+
     plan = group.go(wait=True)
+    origin =  group.get_current_pose()
+    print(origin)
     group.stop()
     group.clear_pose_targets()
 
-  # def pose1(self):
-  #   group = self.group
-  #   group.set_max_acceleration_scaling_factor(0.1)
-  #   group.set_max_velocity_scaling_factor(0.1)
-  #   joint_goal = group.get_current_joint_values()
-  #   joint_goal[0] = -276.88*(3.14/180)
-  #   joint_goal[1] = -28.67*(3.14/180)
-  #   joint_goal[2] = 56.72*(3.14/180)
-  #   joint_goal[3] = -118.02*(3.14/180)
-  #   joint_goal[4] = -89.66*(3.14/180)
-  #   joint_goal[5] = 90*(3.14/180)
-  #   group.go(joint_goal, wait=True)
-  #   group.stop()
-  #   group.clear_pose_targets()
-  #   current_joints = group.get_current_joint_values()
-  #   origin_orientation =  group.get_current_pose().pose.orientation
-  #   origindegree =  euler_from_quaternion([origin_orientation.x, origin_orientation.y, origin_orientation.z, origin_orientation.w]) 
 
-    
-  #   self.origin_degree[0] = origindegree[0]/3.14*180.0
-  #   self.origin_degree[1] = origindegree[1]/3.14*180.0
-  #   self.origin_degree[2] = origindegree[2]/3.14*180.0
-  #   return all_close(joint_goal, current_joints, 0.01)
-
-
-  ###====================== upper cabin ======================###
-  def pose2(self):
+  ###====================== upper cabin(out) ======================###
+  def pose2_1(self):
     group = self.group
     group.set_max_acceleration_scaling_factor(0.1)
     group.set_max_velocity_scaling_factor(0.1)
     joint_goal = group.get_current_joint_values()
-    joint_goal[0] = (103.61 -360)*(3.14/180)
-    joint_goal[1] = -64.36*(3.14/180)
-    joint_goal[2] = 78.97*(3.14/180)
-    joint_goal[3] = -16.05*(3.14/180)
-    joint_goal[4] = 13.62*(3.14/180)
-    joint_goal[5] = -88.53*(3.14/180)
+    joint_goal[0] = 92.57*(3.14/180)
+    joint_goal[1] = -70.72*(3.14/180)
+    joint_goal[2] = 85.4*(3.14/180)
+    joint_goal[3] = -22.13*(3.14/180)
+    joint_goal[4] = 2.59*(3.14/180)
+    joint_goal[5] = -82.54*(3.14/180)
     group.go(joint_goal, wait=True)
     group.stop()
     group.clear_pose_targets()
@@ -182,13 +178,92 @@ class MoveGroupTutorial(object):
     self.origin_degree[2] = origindegree[2]/3.14*180.0
     return all_close(joint_goal, current_joints, 0.01)
 
- ###====================== lower cabin ======================###    
-  def pose3(self):
+
+  ###====================== upper cabin(mid) ======================###
+  def pose2_2(self):
     group = self.group
     group.set_max_acceleration_scaling_factor(0.1)
     group.set_max_velocity_scaling_factor(0.1)
     joint_goal = group.get_current_joint_values()
-    joint_goal[0] = (103.65 -360)*(3.14/180)
+    joint_goal[0] = 97.91*(3.14/180)
+    joint_goal[1] = -67.72*(3.14/180)
+    joint_goal[2] = 81.09*(3.14/180)
+    joint_goal[3] = -15.83*(3.14/180)
+    joint_goal[4] = 7.9*(3.14/180)
+    joint_goal[5] = -87.53*(3.14/180)
+    group.go(joint_goal, wait=True)
+    group.stop()
+    group.clear_pose_targets()
+    current_joints = group.get_current_joint_values()
+    origin_orientation =  group.get_current_pose().pose.orientation
+    origindegree =  euler_from_quaternion([origin_orientation.x, origin_orientation.y, origin_orientation.z, origin_orientation.w]) 
+
+    
+    self.origin_degree[0] = origindegree[0]/3.14*180.0
+    self.origin_degree[1] = origindegree[1]/3.14*180.0
+    self.origin_degree[2] = origindegree[2]/3.14*180.0
+    return all_close(joint_goal, current_joints, 0.01)
+
+
+  ###====================== upper cabin(in) ======================###
+  def pose2_3(self):
+    group = self.group
+    group.set_max_acceleration_scaling_factor(0.1)
+    group.set_max_velocity_scaling_factor(0.1)
+    joint_goal = group.get_current_joint_values()
+    joint_goal[0] = 105.7*(3.14/180)
+    joint_goal[1] = -63.3*(3.14/180)
+    joint_goal[2] = 74.64*(3.14/180)
+    joint_goal[3] = -12.57*(3.14/180)
+    joint_goal[4] = 15.68*(3.14/180)
+    joint_goal[5] = -88.73*(3.14/180)
+    group.go(joint_goal, wait=True)
+    group.stop()
+    group.clear_pose_targets()
+    current_joints = group.get_current_joint_values()
+    origin_orientation =  group.get_current_pose().pose.orientation
+    origindegree =  euler_from_quaternion([origin_orientation.x, origin_orientation.y, origin_orientation.z, origin_orientation.w]) 
+
+    
+    self.origin_degree[0] = origindegree[0]/3.14*180.0
+    self.origin_degree[1] = origindegree[1]/3.14*180.0
+    self.origin_degree[2] = origindegree[2]/3.14*180.0
+    return all_close(joint_goal, current_joints, 0.01)
+
+
+ ###====================== lower cabin(out) ======================###    
+  def pose3_1(self):
+    group = self.group
+    group.set_max_acceleration_scaling_factor(0.1)
+    group.set_max_velocity_scaling_factor(0.1)
+    joint_goal = group.get_current_joint_values()
+    joint_goal[0] = 94.82*(3.14/180)
+    joint_goal[1] = -33.73*(3.14/180)
+    joint_goal[2] = 97.47*(3.14/180)
+    joint_goal[3] = -66.2*(3.14/180)
+    joint_goal[4] = 5.11*(3.14/180)
+    joint_goal[5] = -87.02*(3.14/180)  
+    group.go(joint_goal, wait=True)
+    group.stop()
+    group.clear_pose_targets()
+    current_joints = group.get_current_joint_values()
+    origin_orientation =  group.get_current_pose().pose.orientation
+    origindegree =  euler_from_quaternion([origin_orientation.x, origin_orientation.y, origin_orientation.z, origin_orientation.w]) 
+
+    
+    self.origin_degree[0] = origindegree[0]/3.14*180.0
+    self.origin_degree[1] = origindegree[1]/3.14*180.0
+    self.origin_degree[2] = origindegree[2]/3.14*180.0
+    return all_close(joint_goal, current_joints, 0.01)
+
+
+ ###====================== lower cabin(mid) ======================###    
+  def pose3_2(self):
+    group = self.group
+    group.set_max_acceleration_scaling_factor(0.1)
+    group.set_max_velocity_scaling_factor(0.1)
+    joint_goal = group.get_current_joint_values()
+    joint_goal[0] = 103.65*(3.14/180)
     joint_goal[1] = -31.66*(3.14/180)
     joint_goal[2] = 91.39*(3.14/180)
     joint_goal[3] = -60.7*(3.14/180)
@@ -206,6 +281,33 @@ class MoveGroupTutorial(object):
     self.origin_degree[1] = origindegree[1]/3.14*180.0
     self.origin_degree[2] = origindegree[2]/3.14*180.0
     return all_close(joint_goal, current_joints, 0.01)
+
+
+ ###====================== lower cabin(in) ======================###    
+  def pose3_3(self):
+    group = self.group
+    group.set_max_acceleration_scaling_factor(0.1)
+    group.set_max_velocity_scaling_factor(0.1)
+    joint_goal = group.get_current_joint_values()
+    joint_goal[0] = 102.27*(3.14/180)
+    joint_goal[1] = -32.02*(3.14/180)
+    joint_goal[2] = 92.41*(3.14/180)
+    joint_goal[3] = -61.46*(3.14/180)
+    joint_goal[4] = 12.55*(3.14/180)
+    joint_goal[5] = -88.4*(3.14/180)  
+    group.go(joint_goal, wait=True)
+    group.stop()
+    group.clear_pose_targets()
+    current_joints = group.get_current_joint_values()
+    origin_orientation =  group.get_current_pose().pose.orientation
+    origindegree =  euler_from_quaternion([origin_orientation.x, origin_orientation.y, origin_orientation.z, origin_orientation.w]) 
+
+    
+    self.origin_degree[0] = origindegree[0]/3.14*180.0
+    self.origin_degree[1] = origindegree[1]/3.14*180.0
+    self.origin_degree[2] = origindegree[2]/3.14*180.0
+    return all_close(joint_goal, current_joints, 0.01)  
+
 
  ###====================== place on table ======================###    
   def pose4(self):
@@ -287,31 +389,39 @@ def main():
     print('================================')
     print("pose1")
     tutorial.pose1()
-    # raw_input()
-    # print('================================')
-    # print("pose")
-    # tutorial.pose()
+    raw_input()
+    print('================================')
+    print("pose2_out")
+    tutorial.pose2_1()
+    raw_input()
+    print('================================')
+    print("pose2_in")
+    tutorial.pose2_3()
+    raw_input()
+    print('================================')
+    print("pose2_out")
+    tutorial.pose2_1()
+    raw_input()
+    print('================================')
+    print("pose3_out")
+    tutorial.pose3_1()
+    raw_input()
+    print('================================')
+    print("pose3_in")
+    tutorial.pose3_3()
+    raw_input()
+    print('================================')
+    print("pose3_out")
+    tutorial.pose3_1()
+    raw_input()
+    print('================================')
+    print("pose1")
+    tutorial.pose1()
+    raw_input()
+    print('================================')
+    print("pose")
+    tutorial.pose()
 
-    raw_input()
-    print('================================')
-    print("pose2")
-    tutorial.pose2()
-    raw_input()
-    print('================================')
-    print("pose3")
-    tutorial.pose3()
-    raw_input()
-    print('================================')
-    print("pose")
-    tutorial.pose()
-    raw_input()
-    print('================================')
-    print("pose4")
-    tutorial.pose4()
-    raw_input()
-    print('================================')
-    print("pose")
-    tutorial.pose()
     
     print("============ Python tutorial demo complete!")
     
